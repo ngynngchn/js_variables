9743c5f9707209c63622c7b23109158812245718
Hanni <ngynngchn@gmail.com>
2023-01-03 14:15:51 +0100

    initial commit

---
+++ lev1_6/assets/js/main.js
@@ -0,0 +1,9 @@
+// let x = 34;
+// x = 67;
+// console.log(x);
+// --> können nicht wiederholt deklariert werden
+
+// const y = 34;
+// y = 67;
+// console.log(y);
+// --> Variablen deklariert mit const geben eine Fehlermeldung in der Konsole aus -> const Variablen können nicht verändert werden, da sie eine konstatnte Variable sind.
